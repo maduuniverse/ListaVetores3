1f8e66ddabcc3221a4acb1f20869ed39a2548579
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-08 08:02:18 -0300

    Update AT14.js

--- AT14.js
+++ AT14.js
@@ -1,7 +1,7 @@
-let numeros = [];
+let numeros = [1, 2, 3, 4, 5];
 let soma = 0;
 
-for (let i = 1; i < 5; i += 2) {
+for (let i = 0; i < 5; i++) {
     numeros[i] = Number(prompt("Digite um número:"));
 }
 
