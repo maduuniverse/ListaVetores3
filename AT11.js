919983c090136849c937863cc4fd5cc0d9a00ba5
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-08 07:59:57 -0300

    Create AT11.js

---
+++ AT11.js
@@ -0,0 +1,10 @@
+let numeros = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < 5; i++) {
+    numeros[i] = Number(prompt("Digite um número:"));
+}
+
+let ultimo = numeros.pop();
+numeros.unshift(ultimo);
+
+alert(numeros);
