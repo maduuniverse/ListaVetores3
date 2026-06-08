30b974d1f845f2472f28ec2514b2f1242039a329
Maria Eduarda <0001204539@senaimgaluno.com.br>
2026-06-08 08:01:08 -0300

    Create AT13.js

---
+++ AT13.js
@@ -0,0 +1,12 @@
+let numeros = [1, 2, 3, 4, 5];
+let soma = 0;
+
+for (let i = 0; i < 5; i++) {
+    numeros[i] = Number(prompt("Digite um número:"));
+}
+
+for (let i = 0; i < 5; i += 2) {
+    soma += numeros[i];
+}
+
+alert("Soma das posições pares:", soma);
